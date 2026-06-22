c2f9cdc1b87cca9bcc3c9f6a5c645a3ebedb1714
lx <2012065136@qq.com>
2026-06-22 22:49:49 +0800

    "first commit"

--- worldcoffee-admin/src/main/java/cn/lx/worldcoffee/module/user/domain/User.java
+++ worldcoffee-admin/src/main/java/cn/lx/worldcoffee/module/user/domain/User.java
@@ -3,6 +3,7 @@ package cn.lx.worldcoffee.module.user.domain;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,7 @@ public class User {
     @TableId(type = IdType.AUTO)
     private Long id;
     private String username;
+    @JsonIgnore
     private String password;
     private String phone;
     private Integer status;
