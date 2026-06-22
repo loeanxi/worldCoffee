74fe8fab3fdc0053567848529d30c21819ba9450
lx <2012065136@qq.com>
2026-06-22 10:41:41 +0800

    "first commit"

--- worldcoffee-common/src/main/java/cn/lx/worldcoffee/common/config/OpenApiConfig.java
+++ worldcoffee-common/src/main/java/cn/lx/worldcoffee/common/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package cn.lx.worldcoffee.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,13 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
                 .info(new Info()
                         .title("worldCoffee 接口文档")
                         .version("1.0")
